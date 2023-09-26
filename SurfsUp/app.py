7f87528be081ec02aa5cf975ad1b94948e0161e9
maswanwick <Matt.Swanwick@Cerner.com>
2023-09-26 15:35:09 -0500

    Added the home route

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,6 +1,6 @@
 # Import the dependencies.
 
-
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
@@ -21,10 +21,23 @@
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def home():
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/&lt;start&gt;<br/>"
+        f"/api/v1.0/&lt;start&gt;/&lt;end&gt;<br/>"
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
