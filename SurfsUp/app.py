be4100bd51ae7ed027fef0e887fe079d608d9f44
maswanwick <Matt.Swanwick@Cerner.com>
2023-09-26 16:08:10 -0500

    Added the dynamic date routes

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -118,5 +118,42 @@ def get_temp_observations():
     # Return a list of date/temperature objects as json
     return jsonify(all_temperatures)
 
+@app.route("/api/v1.0/<start>/<end>")
+def get_temp_statistics_range(start, end):
+
+    # Create the base query
+    # Select date and min, avg, and max temperature (tobs) where the observation date is greater than or equal to the provided date
+    base_query = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start)
+
+    # If no end date is provided, use that base query
+    # If an end date is provided, append the end date filter to the existing query object
+    if (end is None):
+        observation_results = base_query
+    else:
+        observation_results = base_query.filter(Measurement.date <= end)
+    
+    # Execute the crafted query, grouped by the date
+    observation_results_by_date = observation_results.group_by(Measurement.date)
+
+    # For each result, create a dictionary object that stores the date, min, avg, and max temperature and append it to the overall list
+    all_observations = []
+    for date, min, avg, max in observation_results_by_date:
+        observation_dict = {}
+        observation_dict['Date'] = date
+        observation_dict['TMIN'] = min
+        observation_dict['TAVG'] = avg
+        observation_dict['TMAX'] = max
+        all_observations.append(observation_dict)
+
+    # Return the list of observations as json
+    return jsonify(all_observations)
+    
+@app.route("/api/v1.0/<start>")
+def get_temp_statistics_start(start):
+
+    # When only receiving the start date, execute get_temp_statistics_range with no end date on the range
+    # get_temp_statistics_range returns json
+    return get_temp_statistics_range(start, None)
+
 if __name__ == '__main__':
     app.run(debug=True)
